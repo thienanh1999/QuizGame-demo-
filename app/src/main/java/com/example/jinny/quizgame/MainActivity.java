27248a7669d0dc73164b26662544187edb27e1fe
Jinny-PC\Jinny <thienanh313@gmail.com>
2018-03-25 15:46:33 +0700

    init project

---
+++ app/src/main/java/com/example/jinny/quizgame/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.jinny.quizgame;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayOptions(actionBar.getDisplayOptions()|actionBar.DISPLAY_SHOW_CUSTOM);
+        actionBar.setCustomView(R.layout.custom_action_bar);
+    }
+}

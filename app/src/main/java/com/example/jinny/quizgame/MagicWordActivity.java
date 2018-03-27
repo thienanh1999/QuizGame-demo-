deb8986125f6a69a419b980de556cf4ae91bc23c
Jinny-PC\Jinny <thienanh313@gmail.com>
2018-03-27 23:42:14 +0700

    thien anh part 2

--- app/src/main/java/com/example/jinny/quizgame/MagicWordActivity.java
+++ app/src/main/java/com/example/jinny/quizgame/MagicWordActivity.java
@@ -1,13 +1,57 @@
 package com.example.jinny.quizgame;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MagicWordActivity extends AppCompatActivity {
+
+    @BindView(R.id.iv_back)
+    ImageView ivBack;
+    @BindView(R.id.tv_point)
+    TextView tvPoint;
+    @BindView(R.id.tv_time)
+    TextView tvTime;
+    @BindView(R.id.iv_image)
+    ImageView ivImage;
+    @BindView(R.id.tv_question)
+    TextView tvQuestion;
+    @BindView(R.id.bt_A)
+    Button btA;
+    @BindView(R.id.bt_B)
+    Button btB;
+    @BindView(R.id.bt_C)
+    Button btC;
+    @BindView(R.id.bt_D)
+    Button btD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_magic_word);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.iv_back, R.id.bt_A, R.id.bt_B, R.id.bt_C, R.id.bt_D})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.iv_back:
+                break;
+            case R.id.bt_A:
+                break;
+            case R.id.bt_B:
+                break;
+            case R.id.bt_C:
+                break;
+            case R.id.bt_D:
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/jinny/quizgame/MainActivity.java
+++ app/src/main/java/com/example/jinny/quizgame/MainActivity.java
@@ -1,18 +1,51 @@
 package com.example.jinny.quizgame;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.cv_flagsGame)
+    CardView cvFlagsGame;
+    @BindView(R.id.cv_magicWord)
+    CardView cvMagicWord;
+    @BindView(R.id.cv_dashboard)
+    CardView cvDashboard;
+    @BindView(R.id.cv_credit)
+    CardView cvCredit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayOptions(actionBar.getDisplayOptions()|actionBar.DISPLAY_SHOW_CUSTOM);
+        actionBar.setDisplayOptions(actionBar.getDisplayOptions() | actionBar.DISPLAY_SHOW_CUSTOM);
         actionBar.setCustomView(R.layout.custom_action_bar);
+    }
+
+    @OnClick({R.id.cv_flagsGame, R.id.cv_magicWord, R.id.cv_dashboard, R.id.cv_credit})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.cv_flagsGame:
+                break;
+            case R.id.cv_magicWord:
+                Intent intent = new Intent(MainActivity.this, MagicWordActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.cv_dashboard:
+                break;
+            case R.id.cv_credit:
+                break;
+        }
     }
 }

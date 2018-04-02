02f09e0afb486fca0c7d5113d6a56ba876725457
Jinny-PC\Jinny <thienanh313@gmail.com>
2018-04-02 23:27:40 +0700

    abc

--- app/src/main/java/com/example/jinny/quizgame/FlagGameActivity.java
+++ app/src/main/java/com/example/jinny/quizgame/FlagGameActivity.java
@@ -12,6 +12,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
